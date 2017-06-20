655bc8405d1f2ebeeea35759a0dd1b03914de448
Bob <bob@bobpalmer.info>
2017-06-19 23:09:11 -0500

    added current HP display, detached camera from player

---
+++ Assets/UI/CameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour {
+
+  public Transform target;
+  public float distance = 0.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+    Debug.Log(target.position);
+		transform.position = new Vector3(target.position.x,target.position.y,target.position.z - distance);
+	}
+}
---
+++ Assets/UI/PlayerHealthUI.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealthUI : MonoBehaviour {
+
+  public GameObject player;
+ 
+  private PlayerHealthHandler playerHealthHandler;
+  private Text text;
+  
+	// Use this for initialization
+	void Start () {
+		playerHealthHandler = player.GetComponent<PlayerHealthHandler>();
+    text = GetComponent<Text>();
+    
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		text.text = "HP:"+playerHealthHandler.health.ToString();
+	}
+}
